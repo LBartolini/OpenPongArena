49cc94ec7f79dc45452ff18c5d5f47a4811bed51
LBartolini <l.bartolini02@gmail.com>
2022-07-27 09:30:40 +0200

    games are stored in db correctly

--- GameServer/src/database.py
+++ GameServer/src/database.py
@@ -26,4 +26,29 @@ class Database():
             return True, float(res[0][1])
 
     def log_game(self, user1: str, user2: str, change1: float, change2: float) -> None:
-        pass
+        with self.database.cursor() as cursor:
+            sql = "INSERT INTO GameLog (PlayerOne, PlayerTwo, ChangeOne, ChangeTwo) VALUES (%s, %s, %s, %s)"
+            cursor.execute(sql, (user1, user2, change1, change2))
+
+            sql = "SELECT ELO FROM User WHERE ID=%s OR ID=%s"
+            cursor.execute(sql, (user1, user2))
+            res = cursor.fetchall()
+
+            elo1, elo2 = res[0][0], res[1][0]
+            
+            sql = "UPDATE User SET ELO=%s WHERE ID=%s"
+            cursor.execute(sql, (elo1+change1, user1))
+            
+            sql = "UPDATE User SET ELO=%s WHERE ID=%s"
+            cursor.execute(sql, (elo2+change2, user2))
+
+            self.database.commit()
+
+
+if __name__ == '__main__':
+    import utils
+
+    env_config: dict = utils.open_environment()
+    database = Database(env_config['DB'])
+    database.log_game(1, 2, 10, -10)
+            
